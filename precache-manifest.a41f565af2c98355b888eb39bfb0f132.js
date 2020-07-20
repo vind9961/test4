d196c120ac0eb8f2d8e2e94cb820d7b2d432e7ee
Vindresh Yadav <coading1245@gmail.com>
2020-07-20 19:58:46 +0530

    Updates

---
+++ precache-manifest.a41f565af2c98355b888eb39bfb0f132.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8d86e54ab5f40960249c1475d19de7",
+    "url": "/test4/index.html"
+  },
+  {
+    "revision": "1376c7f7aca6aa25e2a0",
+    "url": "/test4/static/css/2.1d59c62b.chunk.css"
+  },
+  {
+    "revision": "6c135c4144b388293b6a",
+    "url": "/test4/static/css/main.64c871f9.chunk.css"
+  },
+  {
+    "revision": "1376c7f7aca6aa25e2a0",
+    "url": "/test4/static/js/2.0a92259b.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/test4/static/js/2.0a92259b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c135c4144b388293b6a",
+    "url": "/test4/static/js/main.48643dc8.chunk.js"
+  },
+  {
+    "revision": "295e45a84e96ff344281",
+    "url": "/test4/static/js/runtime-main.3a7eadde.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/test4/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/test4/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/test4/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/test4/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/test4/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
